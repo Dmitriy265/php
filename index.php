445096e392ce2e49503f692cf517a68f7adce612
Dmitriy265 <63237845+Dmitriy265@users.noreply.github.com>
2020-04-17 13:15:48 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,65 +1,116 @@
-<?php
-  require_once('../site_components/header.php');
-?>
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-8">
-      <h1 class="text-center">Узнать ИНН</h1>
-      <form action="inn_obr.php" method="POST">
-        <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-card" aria-hidden="true"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" placeholder="Имя" aria-label="Имя пользователя" aria-describedby="basic-addon1" name="name">
-              </div>
-              <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-card" aria-hidden="true"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" placeholder="Фамилия" aria-label="Имя пользователя" aria-describedby="basic-addon1" name="lastname">
-              </div>
-              <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-card" aria-hidden="true"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" placeholder="Отчество" aria-label="Имя пользователя" aria-describedby="basic-addon1" name="patronymic">
-              </div>
-              <p>Дата рождения</p>
-              <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="basic-addon1"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>
-                  </span>
-                </div>
-                <input type="date" class="form-control" aria-label="Имя пользователя" aria-describedby="basic-addon1" name="birthdate">
-              </div>
-              <p>Номер паспорта</p>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                  <span class="input-group-text" id="basic-addon1"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>
-                  </span>
-                </div>
-                
-                <input type="text" class="form-control" aria-label="Имя пользователя" aria-describedby="basic-addon1" name="docno" placeholder="Номер паспорта">
-              </div>
-             
-              <!-- <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1">@</span>
-              </div>
-              <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="basic-addon1"> // --------------------ОДИН КОНТЕЙНЕР -------------------------//
-              </div> -->
-              <p class="d-none text-danger error-message"></p>
-              <input type="submit" class="btn btn-primary btn-block mt-2 mb-5" value="Узнать ИНН">
-              <!-- <input type="submit" class="btn btn-primary btn-block mt-2" value="Зарегистрироваться">//---КНОПКА СИНЯЯ -->
-      </form>
-    </div>
-  </div>
-</div>
+<?
 
-<?php
-  require_once('../site_components/footer.php');
+header('Content-Type: text/html; charset=utf-8');
+//Инкапсуляция, Носледование, Полиморфизм
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);// --- !!!!!!ПОКАЗЫВАЕТ ОШИБКИ!!!!!!!!-------//
+
+
+class Person {
+  //Модификаторы доступа public, protected, private
+  protected $name;
+  protected $middlename;
+  protected $lastname;
+  protected $partner;
+  protected $father;
+  protected $mother;
+  protected $hp;
   
-?>
+  
+  public function __construct($name, $middlename, $lastname, $father = null, $mother = null ) {// $partner = null - значение по умолчанию
+    $this->name = $name;
+    $this->middlename = $middlename;
+    $this->lastname = $lastname;
+    $this->partner = null;
+    $this->father = $father;
+    $this->mother = $mother; 
+    $this->hp = 100;
+  }
+  public function getName() {
+    return $this->name;
+  }
+  public function setName($name) {
+    $this->name = $name;
+  }// ИНКАПСУЛЯЦИЯ - Заключение в капсулу (чтоб другие не имели досупа для изменений) Василий но выдаст Сергей вместо Василий, потому что Василий защищен//
+
+  public function getMiddlename() {
+    return $this->middlename;
+  }
+  public function getLastname() {
+    return $this->lastname;
+  }
+
+  public function setLastname($lastname) {
+    $this->lastname = $lastname;
+  }
+  public function getPartner() {
+    return $this->partner;
+  }
+  public function setPartner($person) {
+    $this->partner = $person;
+  }
+  
+  public function getFather() {
+    return $this->father;
+  }
+  public function getMother() {
+    return $this->mother; 
+  }
+  public function getHp() {
+    return $this->hp; 
+  }
+  protected function setHp($hp) {
+    if ($hp > 100) {
+      $this->hp =100;
+    } elseif ($hp <= 0) {
+      //$this->hp = 0; 
+      $this->die();
+    } else {
+      $this->hp = $hp;
+    }
+  }
+  public function smoke() { // - 5 hp
+    $this->setHp( $this->getHp() - 5 );
+  }
+  public function takeHealthCare() { // + 10 hp
+    $this->setHp( $this->getHp() + 10 );
+  }
+  protected function die() { 
+    echo $this->getName()." Умер :(";
+  }
+  
+  
+  
+  public function marry($person) {
+    $this->setPartner($person);
+    $person->setPartner($this);
+    $person->setLastname($this->getLastname());
+  }
+  
+}
+
+$vasilisa = new Person("Василиса", "Васильевна", "Придумовна");
+$vasya = new Person("Василий", "Васильевич", "Smith");//vasya - объект класса Person
+//$vasya->setName("Сергей");
+$anna = new Person("Василий", "Васильевич", "Smith", $vasya, $vasilisa);
+//echo $anna->getFather()->getName();
+$vasya->marry($vasilisa);
+
+
+
+$vasya->smoke();
+$vasya->smoke();
+/*$vasya->smoke();
+$vasya->takeHealthCare();*/
+for ($i = 0; $i < 18; $i++) {
+ $vasya->smoke(); 
+}
+//echo $vasya->getHp();
+
+
+//echo $vasya->getName()." ".$vasya->getMiddlename()." ".$vasya->setLastname()."<br>";
+//echo "Жена: ".$vasya->getPartner()->getName()." ".$vasya->getPartner()->getLastname();
+
+
